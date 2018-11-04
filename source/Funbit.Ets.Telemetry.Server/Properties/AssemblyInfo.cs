95cfea4a1d6e06a9a2f36b3aa01c58cb1dde2fa8
iCAROS7 <hominlab@gmail.com>
2018-11-05 03:14:49 +0900

    Modified version to 3.2.5.1

--- source/Funbit.Ets.Telemetry.Server/Properties/AssemblyInfo.cs
+++ source/Funbit.Ets.Telemetry.Server/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Funbit.Ets.Telemetry.Server")]
+[assembly: AssemblyTitle("Funbit.Ets.kor.Telemetry.Server")]
 [assembly: AssemblyDescription("ETS2/ATS Telemetry Server + Mobile Dashboard")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.2.5.0")]
-[assembly: AssemblyFileVersion("3.2.5.0")]
+[assembly: AssemblyVersion("3.2.5.1")]
+[assembly: AssemblyFileVersion("3.2.5.1")]
